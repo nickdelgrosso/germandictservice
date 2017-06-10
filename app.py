cbc85640db2bb42e6bf75f6d95656cc42876b9d2
nickdg <delgrosso@bio.lmu.de>
2017-06-10 19:17:12 +0200

    Takes full text and does comparison on it.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from germandictservice import query_pons_dictionary, extract_definitions, secret
+import re
+from os import path
 
 app = Flask(__name__)
 api = Api(app)
+
+
+with open('wiki_grunwortschatz.txt') as f:
+    common_words = {line.strip().lower(): None for line in f.readlines()}
 
 
 class HelloWorld(Resource):
@@ -17,11 +23,28 @@ class Definitions(Resource):
     def get(self, query):
         response = query_pons_dictionary(query=query, secret=secret)
         result = {}
-        result['definitions'] = list(extract_definitions(response.json()))
-        result['definitions_url'] = 'http://de.pons.com/%C3%BCbersetzung?' + response.url.split('?')[1]
+        result['definition'] = list(extract_definitions(response.json()))[0]['definition']
+        result['definition_url'] = 'http://de.pons.com/%C3%BCbersetzung?' + response.url.split('?')[1]
         return result
 
 api.add_resource(Definitions, '/defs/<string:query>')
+
+
+class AnalyzeText(Resource):
+    def get(self, text):
+        words = re.sub("[^\w]", " ", text).split()
+        words = map(str.lower, words)
+        words_not_found = []
+        for word in words:
+            try:
+                common_words[word]
+            except KeyError:
+                words_not_found.append(word)
+        return words_not_found
+
+api.add_resource(AnalyzeText, '/analyze/<string:text>')
+
+
 
 
 if __name__ == '__main__':
